c58d5e1bc2363b6af52e456c33d850437fa9a4fb
Ankit Jain <ankit.jain67@t-mobile.com>
2019-08-23 12:31:48 -0700

    Displaying the movies from the service

---
+++ video-rental/src/components/movies.jsx
@@ -0,0 +1,43 @@
+import React, { Component } from 'react'
+import { getMovies } from '../services/fakeMovieService'
+
+class Movies extends Component {
+  state = {
+    movies: getMovies()
+  }
+  render () {
+    return (
+      <div>
+        <h2>Movies Component</h2>
+        <table className='table'>
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Genere</th>
+              <th>Stock</th>
+              <th>Rate</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.movies.map(movie => (
+              <tr>
+                <td>{movie.title}</td>
+                <td>{movie.genre.name}</td>
+                <td>{movie.numberInStock}</td>
+                <td>{movie.dailyRentalRate}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    )
+  }
+
+  getMovies = () => {
+    this.state.movies.map(movie => {
+      console.log(movie)
+    })
+  }
+}
+
+export default Movies
