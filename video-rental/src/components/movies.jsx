17c3027223b254f735ce960af29f607d652828c9
Ankit Jain <ankit.jain67@t-mobile.com>
2019-08-23 13:16:57 -0700

    Displaying the movies from the service

--- video-rental/src/components/movies.jsx
+++ video-rental/src/components/movies.jsx
@@ -5,10 +5,20 @@ class Movies extends Component {
   state = {
     movies: getMovies()
   }
+
+  handleDelete = movie => {
+    console.log('Deleted', movie)
+    const movies = this.state.movies.filter(m => m._id !== movie._id)
+    this.setState({ movies })
+  }
   render () {
+    const { length: movieLength } = this.state.movies
+    if (movieLength === 0) {
+      return <p>There are no movies to show</p>
+    }
     return (
       <div>
-        <h2>Movies Component</h2>
+        <p>Showing {movieLength} movies in the database</p>
         <table className='table'>
           <thead>
             <tr>
@@ -16,27 +26,30 @@ class Movies extends Component {
               <th>Genere</th>
               <th>Stock</th>
               <th>Rate</th>
+              <th />
             </tr>
           </thead>
           <tbody>
             {this.state.movies.map(movie => (
-              <tr>
+              <tr key={movie._id}>
                 <td>{movie.title}</td>
                 <td>{movie.genre.name}</td>
                 <td>{movie.numberInStock}</td>
                 <td>{movie.dailyRentalRate}</td>
+                <td>
+                  <button
+                    className='btn btn-danger btn-sm'
+                    onClick={() => this.handleDelete(movie)}
+                  >
+                    Delete
+                  </button>
+                </td>
               </tr>
             ))}
           </tbody>
         </table>
       </div>
     )
-  }
-
-  getMovies = () => {
-    this.state.movies.map(movie => {
-      console.log(movie)
-    })
   }
 }
 
