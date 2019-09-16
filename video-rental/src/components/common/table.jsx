a70e169f668d101c25c77132b34446edfc5b4357
Ankit Jain <ankit.jain67@t-mobile.com>
2019-09-16 14:19:32 -0700

    Adding a different way of object destructuring

--- video-rental/src/components/common/table.jsx
+++ video-rental/src/components/common/table.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import TableHeader from './tableHeader'
 import TableBody from './tableBody'
 
-const Table = props => {
-  const { columns, sortColumn, onSort, data } = props
+const Table = ({ columns, sortColumn, onSort, data }) => {
+  // same as const { columns, sortColumn, onSort, data} = props
   return (
     <table className='table'>
       <TableHeader columns={columns} sortColumn={sortColumn} onSort={onSort} />
